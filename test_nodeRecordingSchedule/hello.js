6064aaaf6be7e99508747f1b8453fb4ba64b992b
Bentleyj <jgbentley10@gmail.com>
2018-04-05 19:03:29 +0100

    Trying with http resquest

--- test_nodeRecordingSchedule/hello.js
+++ test_nodeRecordingSchedule/hello.js
@@ -12,40 +12,44 @@ var password = 'password';
 // var auth = 'Basic' + Buffer.from(username + ':' + password).toString('base64');
 
 
-// var postRequest = {
-// 	host: cam1URL,
-// 	path: "/vapix/services",
-// 	method: "POST",
-// 	headers: {
-// 		'Content-Type': "text/xml; charset=utf-8",
-// 		'SOAPAction': "http://www.axis.com/vapix/ws/action1/GetActionRules"
-// 	}
-// }
-
-// var buffer = "";
-
-// var req = http.request( postRequest, function( res ) {
-// 	console.log( res.statusCode );
-// 	var buffer = "";
-// 	res.on( "data", function( data ) { buffer = buffer + data; } );
-// 	res.on( "end", function( data) { console.log( buffer ); } );
-// });
-
-// req.on('error', function(e) {
-// 	console.log('problem with request: ' + e);
-// });
-
-// req.write(body);
-// req.end();
-
-soap.createClient(actionURL, /*{endpoint: 'http://192.168.100.24/vapix/services'},*/ function(err, client) {
-	if(err) {
-		console.log(err);
+var postRequest = {
+	host: cam1URL,
+	path: "/vapix/services",
+	method: "POST",
+	auth: {
+		user: "root",
+		pass: "password"
+	},
+	headers: {
+		'Content-Type': "text/xml; charset=utf-8",
+		'SOAPAction': "http://www.axis.com/vapix/ws/action1/GetActionRules"
 	}
-	else {
-		console.log(client);
-	}
+}
+
+var buffer = "";
+
+var req = http.request( postRequest, function( res ) {
+	console.log( res.statusCode );
+	var buffer = "";
+	res.on( "data", function( data ) { buffer = buffer + data; } );
+	res.on( "end", function( data) { console.log( buffer ); } );
 });
+
+req.on('error', function(e) {
+	console.log('problem with request: ' + e);
+});
+
+req.write(body);
+req.end();
+
+// soap.createClient(actionURL, /*{endpoint: 'http://192.168.100.24/vapix/services'},*/ function(err, client) {
+// 	if(err) {
+// 		console.log(err);
+// 	}
+// 	else {
+// 		console.log(client.wsdl.descriptions);
+// 	}
+// });
 
 // http.createServer(function (request, response) {
 //     response.writeHead(200, {'Content-Type': 'text/html'});
