00fc16dc6ddd546d98251d33fcb951304e1d650c
Bentleyj <jgbentley10@gmail.com>
2018-04-10 18:02:43 +0100

    Add preliminary #ifdefs for windows port, tested on windows not on unix

--- RTSPControl/RTSPController.cpp
+++ RTSPControl/RTSPController.cpp
@@ -1,27 +1,57 @@
+#include "stdafx.h"
+#ifdef _WIN32
+#include <WinSock2.h>
+#pragma warning(disable:4996)
+#pragma comment(lib, "ws2_32.lib") //Winsock Library
+#else
 #include <stdio.h>
-#include <sys/socket.h>
 #include <stdlib.h>
-#include <netinet/in.h>
-#include <sstream>
 #include <string.h>
 #include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
+#endif
+#include <iostream>
+#include <sstream>
 
 using namespace std;
 
 class RTSPController {
 public:
-	string IPAddress;
-	int port;
-	string session;
-	int client_port;
-	int numMessages;
+	string IPAddress, session;
+	int port, client_port;
+	int valread, numMessages;
+	char buffer[1024];
+	struct sockaddr_in server;
+
+#ifdef _WIN32
+	WSADATA wsa;
+	SOCKET sock;
+#else
+	int sock;
+#endif
 
 	RTSPController() {
+#ifndef _WIN32
 		sock = 0;
+#endif
 		memset(buffer, 0, sizeof(buffer));
 		numMessages = 0;
 	}
+
+#ifdef _WIN32
+	int initializeWinSock() {
+		printf("\nInitialising Winsock...");
+		if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+			printf("Failed. Error Code: %d", WSAGetLastError());
+			return 1;
+		}
+		printf("Initialised.");
+
+		return 0;
+	}
+#endif
 
 	void setIPAndPort(string _IPAddress, int _port, int _clientPort) {
 		IPAddress = _IPAddress;
@@ -29,40 +59,73 @@ public:
 		client_port = _clientPort;
 	}
 
+	int connectToServer() {
+#ifdef _WIN32
+		if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+			printf("Could not create socket : %d", WSAGetLastError());
+		}
+#else
+		if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+			printf("\n Socket creation error \n");
+			return 1;
+		}
+		memset(&server, '0', sizeof(server));
+#endif
+
+		server.sin_addr.s_addr = inet_addr(IPAddress.c_str()); // REMEMBER THIS! IT MIGHT CAUSE AN ERROR!
+		server.sin_family = AF_INET;
+		server.sin_port = htons(port);
+
+#ifndef _WIN32
+		// Convert IPv4 and IPv6 addresses from text to binary form
+		if (inet_pton(AF_INET, IPAddress.c_str(), &server.sin_addr) <= 0) {
+			printf("\nInvalid address/ Address not supported \n");
+			return 1;
+		}
+#endif
+
+		if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+			printf("\nConnection Failed \n");
+			return 1;
+		}
+		return 0;
+	}
+
+	char* sendMessage(string message) {
+		send(sock, message.c_str(), message.length(), 0);
+		numMessages++;
+#ifdef _WIN32
+		valread = recv(sock, buffer, 1024, 0);
+#else
+		valread = read(sock, buffer, 1024);
+#endif
+		printf("%s\n", buffer);
+		return buffer;
+
+	}
+
 	char* sendOptions() {
 		ostringstream oss;
 		oss << "OPTIONS rtsp://" << IPAddress << ":" << port << "/axis-media/media.amp?videocodec=h264&streamprofile=UHDRes RTSP/1.0\r\nCSeq: " << numMessages << "\r\nUser-Agent: Sunrise Master\r\n\r\n";
 		string message = oss.str();
-		send(sock, message.c_str(), message.length(), 0);
-		printf("\nSent Options Request\n");
-		numMessages++;
-		valread = read( sock, buffer, 1024);
-		printf("%s\n", buffer );
-		return buffer;
+		printf("\nSending Options Request\n");
+		return sendMessage(message);
 	}
 
 	char* sendDescribe() {
 		ostringstream oss;
 		oss << "DESCRIBE rtsp://" << IPAddress << ":" << "/axis-media/media.amp?videocodec=h264&streamprofile=UHDRes RTSP/1.0\r\nCSeq: " << numMessages << "\r\nUser-Agent: Sunrise Master\r\nAccept: application/sdp\r\n\r\n";
 		string message = oss.str();
-		send(sock, message.c_str(), message.length(), 0);
-		printf("\nSent Describe Request\n");
-		numMessages++;
-		valread = read( sock, buffer, 1024);
-		printf("%s\n", buffer );
-		return buffer;
+		printf("\nSending Describe Request\n");
+		return sendMessage(message);
 	}
 
 	char* sendSetup() {
 		ostringstream oss;
-		oss << "SETUP rtsp://" << IPAddress << ":" << "/axis-media/media.amp?videocodec=h264&streamprofile=UHDRes RTSP/1.0\r\nCSeq: " << numMessages << "\r\nTransport: RTP/AVP;unicast;client_port=" << client_port << "-" << client_port+1<<"\r\n\r\n";
+		oss << "SETUP rtsp://" << IPAddress << ":" << "/axis-media/media.amp?videocodec=h264&streamprofile=UHDRes RTSP/1.0\r\nCSeq: " << numMessages << "\r\nTransport: RTP/AVP;unicast;client_port=" << client_port << "-" << client_port + 1 << "\r\n\r\n";
 		string message = oss.str();
-		send(sock, message.c_str(), message.length(), 0);
-		printf("\nSent Setup Request\n");
-		numMessages++;
-		valread = read( sock, buffer, 1024);
-		printf("%s\n", buffer );
-		return buffer;
+		printf("\nSending Setup Request\n");
+		return sendMessage(message);
 	}
 
 	char* setupSession() {
@@ -75,59 +138,24 @@ public:
 		ostringstream oss;
 		oss << "PLAY rtsp://" << IPAddress << ":" << "/axis-media/media.amp?videocodec=h264&streamprofile=UHDRes RTSP/1.0\r\nCSeq: " << numMessages << "\r\nSession: " << session << "\r\nRange: npt=0.000-\r\n\r\n";
 		string message = oss.str();
-		send(sock, message.c_str(), message.length(), 0);
-		printf("\nSent Play Request\n");
-		numMessages++;
-		valread = read( sock, buffer, 1024);
-		printf("%s\n", buffer );
-		return buffer;
+		printf("\nSending Play Request\n");
+		return sendMessage(message);
 	}
 
 	char* sendPause() {
 		ostringstream oss;
-		oss << "PAUSE rtsp://" << IPAddress << ":" << port << "/axis-media/media.amp?videocodec=h264&streamprofile=UHDRes RTSP/1.0\r\nCSeq: " << numMessages << "\r\nSession: " << "\r\n\r\n";
+		oss << "PAUSE rtsp://" << IPAddress << ":" << port << "/axis-media/media.amp?videocodec=h264&streamprofile=UHDRes RTSP/1.0\r\nCSeq: " << numMessages << "\r\nSession: " << session << "\r\n\r\n";
 		string message = oss.str();
-		send(sock, message.c_str(), message.length(), 0);
-		printf("\nSent Pause Request\n");
-		numMessages++;
-		valread = read( sock, buffer, 1024);
-		printf("%s\n", buffer );
-		return buffer;
+		printf("\nSending Pause Request\n");
+		return sendMessage(message);
 	}
 
 	char* sendTeardown() {
 		ostringstream oss;
-		oss << "TEARDOWN rtsp://" << IPAddress << ":" << port << "/axis-media/media.amp?videocodec=h264&streamprofile=UHDRes RTSP/1.0\r\nCSeq: " << numMessages << "\r\nSession: " << "\r\n\r\n";
+		oss << "TEARDOWN rtsp://" << IPAddress << ":" << port << "/axis-media/media.amp?videocodec=h264&streamprofile=UHDRes RTSP/1.0\r\nCSeq: " << numMessages << "\r\nSession: " << session << "\r\n\r\n";
 		string message = oss.str();
-		send(sock, message.c_str(), message.length(), 0);
-		printf("\nSent Teardown Request\n");
-		numMessages = 0;
-		valread = read( sock, buffer, 1024);
-		printf("%s\n", buffer );
-		return buffer;
-	}
-
-	int connectToServer() {
-		if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-			printf("\n Socket creation error \n");
-			return -1;
-		}
-
-		memset(&serv_addr, '0', sizeof(serv_addr));
-
-		serv_addr.sin_family = AF_INET;
-		serv_addr.sin_port = htons(port);
-
-		// Convert IPv4 and IPv6 addresses from text to binary form
-		if(inet_pton(AF_INET, IPAddress.c_str(), &serv_addr.sin_addr)<=0) {
-			printf("\nInvalid address/ Address not supported \n");
-			return -1;
-		}
-		if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-			printf("\nConnection Failed \n");
-			return -1;
-		}
-		return 1;
+		printf("\nSending Teardown Request\n");
+		return sendMessage(message);
 	}
 
 	void setSessionIDFromResponseBuffer(char* response) {
@@ -137,18 +165,12 @@ public:
 
 		string sessionID = "";
 
-		for(int i = s + identifier.length() - response; i < 1024; i++) {
-			if(response[i] == ';')
+		for (int i = s + identifier.length() - response; i < 1024; i++) {
+			if (response[i] == ';')
 				break;
 			sessionID += response[i];
 		}
 
 		session = sessionID;
 	}
-
-private:
-	struct sockaddr_in address;
-	int sock, valread;
-	struct sockaddr_in serv_addr;
-	char buffer[1024];
 };
--- RTSPControlWin/RTSPTest/RTSPTest/RTSPTest.cpp
+++ RTSPControlWin/RTSPTest/RTSPTest/RTSPTest.cpp
@@ -8,18 +8,45 @@
 
 using namespace std;
 
-
 int main()
 {
-	RTSPController controller;
+	RTSPController controller24, controller34;
 
-	controller.initializeWinSock();
+	controller24.initializeWinSock();
 
-	controller.createSocket();
+	controller24.setIPAndPort("192.168.100.24", 554, 100);
 
-	controller.setServerIPAndPort("192.168.100.24", 554, 3000);
+	controller34.initializeWinSock();
 
-	controller.connectToRemoteServer();
+	controller34.setIPAndPort("192.168.100.34", 554, 200);
+
+	controller24.connectToServer();
+
+	controller24.sendOptions();
+
+	controller24.sendDescribe();
+
+	controller24.setupSession();
+
+	controller24.sendPlay();
+
+	controller24.sendPause();
+
+	controller24.sendTeardown();
+
+	controller34.connectToServer();
+
+	controller34.sendOptions();
+
+	controller34.sendDescribe();
+
+	controller34.setupSession();
+
+	controller34.sendPlay();
+
+	controller34.sendPause();
+
+	controller34.sendTeardown();
 
     return 0;
 }
