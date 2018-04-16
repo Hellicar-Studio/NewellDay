daa380ce6545c9de8644411bc68794c427029f52
Bentleyj <jgbentley10@gmail.com>
2018-04-16 18:17:12 +0100

    Add better control for range of cameras accessed

--- RTSPControlWin/RTSPTest/RTSPTest/RTSPTest.cpp
+++ RTSPControlWin/RTSPTest/RTSPTest/RTSPTest.cpp
@@ -5,45 +5,53 @@
 #include <iostream>
 
 #include "RTSPController.cpp"
+#include "SunriseFinder.cpp"
+
+#define START 0
+#define STOP 4
 
 using namespace std;
 
 int main()
 {
 	// Test 123
+	SunriseFinder sunriseFinder;
+	sunriseFinder.setTime();
 	RTSPController rtspController[4]; // These cameras listen for RTSP by default on port 554 and for HTTP by default on port 80
 	RTSPControllerListener rtspControllerListener[4];
 
 	rtspController[0].setIPAndPort("192.168.0.41", 554, 1042);
-	rtspController[0].addListener(&rtspControllerListener[0]);
-	rtspController[1].setIPAndPort("192.168.0.51", 554, 52);
-	rtspController[2].setIPAndPort("192.168.0.61", 554, 62);
-	rtspController[3].setIPAndPort("192.168.0.71", 554, 72);
+	rtspController[1].setIPAndPort("192.168.0.51", 554, 1052);
+	rtspController[2].setIPAndPort("192.168.0.61", 554, 1062);
+	rtspController[3].setIPAndPort("192.168.0.71", 554, 1072);
 
-	for (int i = 0; i < 1; i++) {
+	for (int i = START; i < STOP; i++) {
+		rtspController[i].addListener(&rtspControllerListener[i]);
+	}
+
+	for (int i = START; i < STOP; i++) {
 		rtspController[i].initializeWinSock();
 		rtspController[i].connectToServer();
 	}
 
-	for (int i = 0; i < 1; i++) {
+	for (int i = START; i < STOP; i++) {
 		rtspController[i].sendOptions();
 		rtspController[i].sendDescribe();
 		rtspController[i].setupSession();
 		rtspController[i].sendPlay();
 	}
 
-
 	printf("\nStreams Started, press any key to ask for options again.\n");
 	system("pause");
 
-	for (int i = 0; i < 1; i++) {
+	for (int i = START; i < STOP; i++) {
 		rtspController[i].sendOptions();
 	}
 
 	printf("\nStreams Alive, press any key to stop them.\n");
 	system("pause");
 
-	for (int i = 0; i < 1; i++) {
+	for (int i = START; i < STOP; i++) {
 		rtspController[i].sendPause();
 		rtspController[i].sendTeardown();
 	}
